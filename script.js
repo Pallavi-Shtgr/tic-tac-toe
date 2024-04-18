132fb2b4fce23718c37582a625f7ba814775ff94
Pallavi <pallavishettigar03@gmail.com>
2024-04-18 21:29:02 +0530

    add changes

--- script.js
+++ script.js
@@ -30,3 +30,52 @@ playerOIcon = "far fa-circle",
 playerSign = "X",
 runBot = true;
 
+function clickedBox(element){
+    if(players.classList.contains("player")){
+        playerSign = "O";
+        element.innerHTML = `<i class="${playerOIcon}"></i>`;
+        players.classList.remove("active");
+        element.setAttribute("id", playerSign);
+    }else{
+        element.innerHTML = `<i class="${playerXIcon}"></i>`;
+        element.setAttribute("id", playerSign);
+        players.classList.add("active");
+    }
+    selectWinner();
+    element.style.pointerEvents = "none";
+    playBoard.style.pointerEvents = "none";
+    let randomTimeDelay = ((Math.random() * 1000) + 200).toFixed();
+    setTimeout(()=>{
+        bot(runBot);
+    }, randomTimeDelay);
+}
+
+function bot(){
+    let array = [];
+    if(runBot){
+        playerSign = "O";
+        for (let i = 0; i < allBox.length; i++) {
+            if(allBox[i].childElementCount == 0){
+                array.push(i);
+            }
+        }
+        let randomBox = array[Math.floor(Math.random() * array.length)];
+        if(array.length > 0){
+            if(players.classList.contains("player")){ 
+                playerSign = "X";
+                allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`;
+                allBox[randomBox].setAttribute("id", playerSign);
+                players.classList.add("active");
+            }else{
+                allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`;
+                players.classList.remove("active");
+                allBox[randomBox].setAttribute("id", playerSign);
+            }
+            selectWinner();
+        }
+        allBox[randomBox].style.pointerEvents = "none";
+        playBoard.style.pointerEvents = "auto";
+        playerSign = "X";
+    }
+}
+
